ce38270f8b5b09dc322f09847b8cc54a11435a46
Nishath-Syed <nishathsyed95@gmail.com>
2025-03-16 14:37:09 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());  
 app.use(express.json());  
 app.use(cors({
-  origin: 'http://localhost:5173',  
+  origin: 'https://67d6930f7a0d6331128eaadc--remarkable-bunny-673cb8.netlify.app',  
   methods: 'GET,POST,PUT,DELETE',
   allowedHeaders: 'Content-Type,Authorization',
   credentials: true
